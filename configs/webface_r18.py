224c9ef80ca8e9e848ab20fa4b329b3ef1e660c3
agata migalska <pimeyes@alphamoon.ai>
2021-10-14 11:29:39 +0200

    feat: run on deepstributed servers

---
+++ configs/webface_r18.py
@@ -0,0 +1,28 @@
+from easydict import EasyDict as edict
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.loss = "arcface"
+config.network = "r18"
+config.resume = False
+config.output = "/output/webface_arcface_r18"
+
+config.dataset = "webface"
+config.embedding_size = 512
+config.sample_rate = 1
+config.fp16 = False
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 64
+config.lr = 0.1  # batch size is 512
+
+config.rec = "/data"
+config.num_classes = 10572
+config.num_image = "forget"
+config.num_epoch = 1  # było 34
+config.warmup_epoch = -1
+config.decay_epoch = [20, 28, 32]
+config.val_targets = ["lfw", "cfp_fp", "agedb_30"]
