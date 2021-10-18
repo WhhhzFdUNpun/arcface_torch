f4ff4809fed2a2ec77a4518b04dec52ed027bfa2
agata migalska <pimeyes@alphamoon.ai>
2021-10-18 15:31:43 +0200

    feat: arcface predictor

--- inference.py
+++ inference.py
@@ -1,8 +1,13 @@
 import argparse
+from pathlib import Path
 
 import cv2
 import numpy as np
 import torch
+import torchvision.transforms as tt
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from tqdm import tqdm
 
 from backbones import get_model
 
@@ -23,7 +28,49 @@ def inference(weight, name, img):
     net.load_state_dict(torch.load(weight))
     net.eval()
     feat = net(img).numpy()
-    print(feat)
+    return feat
+
+
+class ArcfacePredictor:
+    def __init__(self, model_name: str, weight: Path, data_path: Path = None, device='cpu'):
+        """
+
+        Args:
+            model_name: r18, r34, r50, r100, r200, r2060, mbf
+            weight:
+            data_path:
+            device:
+        """
+        self.model = get_model(model_name, fp16=False)
+        self.model.load_state_dict(torch.load(weight))
+        self.model.eval()
+
+        self.transform = tt.Compose([
+            tt.Resize(112, interpolation=tt.InterpolationMode.BICUBIC),
+            tt.CenterCrop(100),
+            tt.ToTensor(),
+            tt.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+        self.device = device
+        self.model.to(self.device)
+        if data_path is not None:
+            dataset = datasets.ImageFolder(str(data_path), transform=self.transform)
+            self.data_loader = DataLoader(
+                dataset=dataset,
+                batch_size=64,
+                num_workers=4,
+            )
+
+    @torch.no_grad()
+    def __call__(self, image):
+        return self.model(image.to(self.device)).cpu().numpy()
+
+    @torch.no_grad()
+    def run(self):
+        embeddings = []
+        for batch, idx in tqdm(self.data_loader, total=len(self.data_loader)):
+            embeddings.append(self.model(batch))
+        return torch.hstack(embeddings)
 
 
 if __name__ == "__main__":
@@ -32,4 +79,5 @@ if __name__ == "__main__":
     parser.add_argument('--weight', type=str, default='')
     parser.add_argument('--img', type=str, default=None)
     args = parser.parse_args()
-    inference(args.weight, args.network, args.img)
+    feat = inference(args.weight, args.network, args.img)
+    print(feat)
