ad60ba19a008ff960d5ab9d4ed5684ba48c2820a
agata migalska <pimeyes@alphamoon.ai>
2021-10-27 18:00:02 +0200

    chore: resnet50 config

--- configs/webface_r50_2048.py
+++ configs/webface_r50_2048.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from easydict import EasyDict as edict
 
 # make training faster
@@ -8,21 +10,21 @@ config = edict()
 config.loss = "arcface"
 config.network = "r50"
 config.resume = False
-config.output = "/output/webface_r50_512"
+config.output = f"/output/{Path(__file__).resolve().stem}"
 
 config.dataset = "webface"
-config.embedding_size = 512
+config.embedding_size = 2048
 config.sample_rate = 1
 config.fp16 = False
 config.momentum = 0.9
 config.weight_decay = 5e-4
-config.batch_size = 64
-config.lr = 0.1  # batch size is 512
+config.batch_size = 32
+config.lr = 1  # batch size is 512
 
 config.rec = "/data"
 config.num_classes = 10572
 config.num_image = "forget"
-config.num_epoch = 50
+config.num_epoch = 20
 config.warmup_epoch = -1
 config.decay_epoch = [20, 30, 40]
-config.val_targets = ["lfw", "cfp_fp", "agedb_30"]
+config.val_targets = ["agedb_30", "dev_00", "dev_04"]
