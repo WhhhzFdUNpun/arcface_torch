4e1b54b39c9303b6d3373cbf5a7a48616c02d3d9
agata migalska <pimeyes@alphamoon.ai>
2021-10-28 18:37:37 +0200

    chore: resnet50 config

---
+++ configs/webface_r50_2048_2.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+
+from easydict import EasyDict as edict
+
+# make training faster
+# our RAM is 256G
+# mount -t tmpfs -o size=140G  tmpfs /train_tmp
+
+config = edict()
+config.loss = "arcface"
+config.network = "r50"
+config.resume = True
+config.resume_file = '/pretrained/backbone_phase1.pth'
+config.output = f"/output/{Path(__file__).resolve().stem}"
+
+config.dataset = "webface"
+config.embedding_size = 2048
+config.sample_rate = 1
+config.fp16 = False
+config.momentum = 0.9
+config.weight_decay = 5e-4
+config.batch_size = 32
+config.lr = 0.1  # batch size is 512
+
+config.rec = "/data"
+config.num_classes = 10572
+config.num_image = "forget"
+config.num_epoch = 10
+config.warmup_epoch = -1
+config.decay_epoch = []
+config.val_targets = ["agedb_30", "dev_00", "dev_04"]
