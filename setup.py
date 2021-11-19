df6de7c74e0c59c6384c44b7e9a6fccd491e0883
agata migalska <pimeyes@alphamoon.ai>
2021-11-19 12:11:57 +0100

    chore: some attempts to learn on the augmented set

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arcface-torch',
+    version='1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Alphamoon sp. z o.o.',
+    author_email='',
+    description='Fork of '
+                'https://github.com/deepinsight/insightface/tree/master/recognition/arcface_torch'
+)
